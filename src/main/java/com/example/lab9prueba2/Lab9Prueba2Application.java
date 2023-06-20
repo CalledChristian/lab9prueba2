c6c0e2d42b357b56dfa7526e9fb89d7bb3154070
Milene <a20206456@pucp.edu.pe>
2023-06-20 17:19:27 -0500

    commit 1

---
+++ src/main/java/com/example/lab9prueba2/Lab9Prueba2Application.java
@@ -0,0 +1,13 @@
+package com.example.lab9prueba2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab9Prueba2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab9Prueba2Application.class, args);
+    }
+
+}
